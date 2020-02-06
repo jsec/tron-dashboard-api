f906e152a1b8f37cc36ac161acdb5eb3ffb1f595
Jarrod Seccombe <jarrod@cynical.software>
2020-02-06 18:22:51 -0500

    add logging middleware

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,11 +1,17 @@
-import { Module } from "@nestjs/common";
+import { Module, MiddlewareConsumer } from "@nestjs/common";
 import { AudioModule } from "./audio.module";
 import { GitModule } from "./git.module";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { MorganMiddleware } from "@nest-middlewares/morgan";
 
 @Module({
   imports: [TypeOrmModule.forRoot(), AudioModule, GitModule],
   controllers: [],
   providers: []
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    MorganMiddleware.configure("dev");
+    consumer.apply(MorganMiddleware).forRoutes("/");
+  }
+}
