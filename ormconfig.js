307e33811e94ac69de561de61551626f755126d8
Jarrod Seccombe <jarrod@cynical.software>
2020-02-06 18:24:10 -0500

    decrease sqlite logging level

--- ormconfig.js
+++ ormconfig.js
@@ -2,7 +2,7 @@ module.exports = {
   type: "sqlite",
   database: "./data/audio.sqlite",
   entities: ["dist/models/**/*.js"],
-  logging: true,
+  logging: false,
   synchronize: true,
   seeds: ["src/seeds/**/*.ts"]
 };
