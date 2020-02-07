cdbc07bb37d5ac0fb13e2eb0953847686466a7bc
Jarrod Seccombe <jarrod@cynical.software>
2020-02-06 19:45:22 -0500

    strongly type params arguments

--- src/controllers/audio.controller.ts
+++ src/controllers/audio.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get, Param, Post, Delete } from "@nestjs/common";
 import { AudioService } from "../services/audio.service";
 import Audio from "../models/audio.model";
+import { AudioRequest } from "../interfaces/audio-request.interface";
+import { DeleteResult } from "typeorm";
 
 @Controller("audio")
 export class AudioController {
@@ -12,7 +14,7 @@ export class AudioController {
   }
 
   @Get(":id")
-  async getAudio(@Param() params): Promise<void> {
+  async getAudio(@Param() params: AudioRequest): Promise<void> {
     return this.audioService.playAudio(params.id);
   }
 
@@ -22,7 +24,7 @@ export class AudioController {
   }
 
   @Delete(":id")
-  async deleteAudio(@Param() params): Promise<void> {
+  async deleteAudio(@Param() params: AudioRequest): Promise<DeleteResult> {
     return this.audioService.deleteAudio(params.id);
   }
 }
